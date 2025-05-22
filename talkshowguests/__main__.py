47395e5c99fec6b74608d1e9438e01088e1a53fe
Lukas Stratmann <stratmann@ccs-labs.org>
2025-05-22 22:39:58 +0200

    Assemble report message

--- talkshowguests/__main__.py
+++ talkshowguests/__main__.py
@@ -5,6 +5,7 @@ import json
 import os
 import pathlib
 
+from dotenv import load_dotenv
 import pandas as pd
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
@@ -38,6 +39,11 @@ def main():
     )
     args = parser.parse_args()
 
+    # Environment variables can be either passed as regular
+    # environment variables or as a .env file in the same
+    # folder:
+    load_dotenv()
+
     settings = get_project_settings()
     settings.set("FEED_FORMAT", "jsonlines")
     settings.set("FEED_URI", str(args.crawler_results))
@@ -47,7 +53,7 @@ def main():
         process.crawl(spider)
     process.start()
 
-    with open("result.jsonlines", "r") as f:
+    with open(args.crawler_results, "r") as f:
         results = [TalkshowItem(**json.loads(line)) for line in f]
 
         # print("\n\n\nHere come the results:")
@@ -79,11 +85,11 @@ def main():
         api_token = os.getenv("TELEGRAM_API_TOKEN")
         chat_id = os.getenv("TELEGRAM_CHAT_ID")
         if api_token and chat_id:
-            asyncio.run(report_episodes_telegram(
+            report_episodes_telegram(
                 episodes=episodes_to_report,
                 api_token=api_token,
                 chat_id=chat_id,
-            ))
+            )
         else:
             raise ValueError(
                 "Missing TELEGRAM_API_TOKEN or TELEGRAM_CHAT_ID"
@@ -99,14 +105,35 @@ def main():
     new_history.to_csv(args.history_file)
 
 
-async def report_episodes_telegram(
-    episodes: TalkshowItem,
+def report_episodes_telegram(
+    episodes: list[TalkshowItem],
     api_token: str,
     chat_id: str,
 ):
-    bot = telegram.Bot(api_token)
-    async with bot:
-        await bot.send_message(
-            text="Hello world",
-            chat_id=chat_id,
+    if not episodes:
+        return
+
+    async def _send_message(msg: str):
+        bot = telegram.Bot(api_token)
+        async with bot:
+            await bot.send_message(
+                text=msg,
+                chat_id=chat_id,
+                parse_mode="MarkdownV2",
+            )
+
+    msg = "*Neue Talkshow-Folgen:*\n\n"
+    for episode in episodes:
+        msg += datetime.datetime.fromisoformat(episode["isodate"]).strftime(
+            "%Y-%m-%d"  # We don't want hours and minutes for now
         )
+        msg += (
+            f" *{episode['name']}*\n"
+            f"Gäste:\n"
+            + "\n".join([f"- {g}" for g in episode['guests']])
+            + (f"Thema: _{episode['topic']}_" if episode["topic"] else "")
+            + "\n\n"
+        )
+    msg = msg.replace("-", "\-")
+    print(msg)
+    asyncio.run(_send_message(msg))
