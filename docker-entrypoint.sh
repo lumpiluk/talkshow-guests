e1a472cbb1019402ff83552839663413d8e7410f
Lukas Stratmann <stratmann@ccs-labs.org>
2025-05-25 12:24:25 +0200

    Docker: Make the crawl schedule configurable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,21 +7,30 @@ IFS=$'\n\t'
 HOST_UID=$(stat -c '%u' /data)
 HOST_GID=$(stat -c '%g' /data)
 
+user_exists(){ id "$1" &>/dev/null; }
+
 if [ "$HOST_UID" -ne 0 ]; then
-    addgroup --gid $HOST_GID hostgroup
-    adduser --uid $HOST_UID \
-        --gid $HOST_GID \
-        --disabled-password \
-        --no-create-home \
-        --gecos "" \
-        hostuser
+    if ! user_exists hostuser; then
+        addgroup --gid $HOST_GID hostgroup
+        adduser --uid $HOST_UID \
+            --gid $HOST_GID \
+            --disabled-password \
+            --no-create-home \
+            --gecos "" \
+            hostuser
+    fi
     CRON_USER=hostuser
+    echo "Running with UID $HOST_UID based on ownership of the /data directory."
 else
     echo "Running as root based on ownership of the /data directory."
     CRON_USER=root
 fi
 
-echo "*/20 * * * * $CRON_USER /run.sh >> /var/log/cron.log 2>&1" \
+default_schedule='*/20 * * * *'
+CRAWL_SCHEDULE="${CRAWL_SCHEDULE:-$default_schedule}"
+echo "Will crawl talkshows using this schedule: $CRAWL_SCHEDULE"
+
+echo "$CRAWL_SCHEDULE $CRON_USER /run.sh >> /var/log/cron.log 2>&1" \
     > /etc/cron.d/talkshowguests
 chmod 0644 /etc/cron.d/talkshowguests
 touch /var/log/cron.log
